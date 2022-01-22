068c13538aa61093925ef0a249aab6f6de4f5236
Henrique Klug <henriqueklug@gmail.com>
2022-01-21 23:51:14 -0300

    v1.1.1

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const Storage = {
 }
 
 const Transactions = {
-    all: transaction = Storage.get(),
+    all: Storage.get(),
 
     createTransactions(index) {
         const tr = document.createElement('tr');
@@ -18,6 +18,7 @@ const Transactions = {
             <td class="description">${Transactions.all[index].description}</td>
             <td class="${Transactions.all[index].amount > 0 ? 'income' : 'expense'}">${Util.formatCurrency(Transactions.all[index].amount)}</td>
             <td class="date">${Transactions.all[index].date}</td>
+            <td><img src="assets/plus.svg" alt="plus icon" onclick="Transactions.updateTransaction(${index})" title="update"></td>
             <td><img src="assets/minus.svg" alt="minus icon" onclick="Transactions.removeTransaction(${index})" title="remove"></td>
             `
         document.querySelector('tbody').appendChild(tr);
@@ -25,8 +26,18 @@ const Transactions = {
 
     addTransaction(transaction) {
         Transactions.all.push(transaction);
+    },
 
-        App.reload();
+    updateTransaction(index) {
+        Form.toggleModalForUpdate();
+        document.querySelector('#descriptionUpdate').value = Transactions.all[index].description;
+        document.querySelector('#amountUpdate').value = Util.reFormatCurrency(Transactions.all[index].amount);
+        document.querySelector('#dateUpdate').value = Util.reFormatDate(Transactions.all[index].date);
+        Form.updateIndex = index;
+    },
+
+    addUpdateTransaction(transaction, index) {
+        Transactions.all[index] = transaction;
     },
 
     removeTransaction(index) {
@@ -79,8 +90,14 @@ const Form = {
     amount: document.querySelector('#amount'),
     date: document.querySelector('#date'),
 
+    updateIndex: 0,
+
     toggleModal() {
         document.querySelector('.modal-overlay').classList.toggle('active');
+    },
+
+    toggleModalForUpdate() {
+        document.querySelector('.modal-overlay.update').classList.toggle('active');
     },
 
     getValues() {
@@ -88,6 +105,14 @@ const Form = {
             description: Form.description.value,
             amount: Util.formatAmount(Form.amount.value),
             date: Util.formatDate(Form.date.value),
+        }
+    },
+
+    getUpdatedValues() {
+        return {
+            description: document.querySelector('#descriptionUpdate').value,
+            amount: Util.formatAmount(document.querySelector('#amountUpdate').value),
+            date: Util.formatDate(document.querySelector('#dateUpdate').value),
         }
     },
 
@@ -106,7 +131,17 @@ const Form = {
         Form.clearForm();
         Form.toggleModal();
         App.reload();
-    }
+    },
+
+    submitFormForUpdate(event) {
+        event.preventDefault();
+
+        const updatedTransaction = Form.getUpdatedValues();
+        Transactions.addUpdateTransaction(updatedTransaction, Form.updateIndex);
+
+        Form.toggleModalForUpdate();
+        App.reload();
+    },
 }
 
 const Util = {
@@ -132,13 +167,22 @@ const Util = {
     formatDate(date) {
         date = date.split('-').reverse();
         return String(date).replace(/\,/g, '/');
-    }
+    },
+
+    reFormatCurrency(value) {
+        return value / 100;
+    },
+
+    reFormatDate(date) {
+        date = date.split('/').reverse();
+        return String(date).replace(/\,/g, '-');
+    },
 }
 
 const App = {
     init() {
         Transactions.all.forEach(transactionObj => {
-            Transactions.createTransactions(transaction.indexOf(transactionObj))
+            Transactions.createTransactions(Transactions.all.indexOf(transactionObj))
         });
         Balance.updateBalance();
         Storage.set(Transactions.all);
@@ -149,4 +193,4 @@ const App = {
     },
 }
 
-App.init(); 
+App.init();
